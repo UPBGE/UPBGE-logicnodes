3d25e59902736c32d97d0e43949f310e934bbcab
Leo <lauersperg@gmx.at>
2020-01-15 14:40:58 +0100

    And Not

--- __init__.py
+++ __init__.py
@@ -8,7 +8,7 @@ bl_info = {
     "name": "Uchronian Logic - UPBGE Logic Nodes",
     "description": "A Node System to create game logic.",
     "author": "pgi, Leopold A-C (Iza Zed)",
-    "version": (0, 3, 6),
+    "version": (0, 3, 7),
     "blender": (2, 82, 0),
     "location": "View Menu",
     "warning": "Alpha",
