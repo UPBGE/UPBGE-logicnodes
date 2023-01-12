88a3684fc447a327300c77d695ccc3461b3e4765
Leo <leo@auersperg.org>
2023-01-12 11:06:06 +0100

    version

--- __init__.py
+++ __init__.py
@@ -15,7 +15,7 @@ bl_info = {
         "A Node System to create game logic."
     ),
     "author": "pgi, Leopold A-C (Iza Zed)",
-    "version": (2, 1, 6),
+    "version": (2, 1, 6, 1),
     "blender": (2, 91, 0),
     "location": "View Menu",
     "category": "Game Engine"
