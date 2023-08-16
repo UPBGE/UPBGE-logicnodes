4cd7d12324eac23f8d5e8acccd2204fed7349227
Teck-Freak <der_loefe@yahoo.de>
2023-08-16 23:38:13 +0200

    Minor cleanup on enumitems: WTH are there enum-keys that are numbers or constants _with offsets_?! That should be refactored!

--- basicnodes/__init__.py
+++ basicnodes/__init__.py
@@ -94,7 +94,6 @@ _enum_mouse_wheel_direction = [
     ("3", "Scroll Up or Down", "Mouse Wheel Scrolled either Up or Down[3]")
 ]
 
-
 _enum_vector_math_options = [
     ("scale", "Scale", "A multiplied by Scale"),
     ("length", "Length", "Length of A"),
@@ -120,7 +119,6 @@ _enum_vector_math_options = [
     ("negate", "Negate", "Multiply all values by -1")
 ]
 
-
 _enum_type_casts = [
     ("int", "To Integer", "Convert this value to an integer type"),
     ("bool", "To Boolean", "Convert this value to a boolean type"),
@@ -128,26 +126,24 @@ _enum_type_casts = [
     ("float", "To Float", "Convert this value to a float type")
 ]
 
-
 _enum_distance_models = [
     ('INVERSE', 'Inverse', 'Sound will fade exponentially (Realistic)'),
     ('INVERSE_CLAMPED', 'Inverse Clamped',
-     'Sound will fade exponentially (Realistic, Clamped)'),
-    ('EXPONENT', 'Exponent', 'Sound will fade detemined by an exponent (Good audibility)'),
+        'Sound will fade exponentially (Realistic, Clamped)'),
+    ('EXPONENT', 'Exponent',
+        'Sound will fade detemined by an exponent (Good audibility)'),
     ('EXPONENT_CLAMPED', 'Exponent Clamped',
-     'Sound will fade detemined by an exponent (Good audibility, Clamped)'),
+        'Sound will fade detemined by an exponent (Good audibility, Clamped)'),
     ('LINEAR', 'Linear', 'Sound will fade in a linear relation to distance'),
     ('LINEAR_CLAMPED', 'Linear Clamped',
-     'Sound will fade in a linear relation to distance (Clamped)'),
+        'Sound will fade in a linear relation to distance (Clamped)'),
     ('NONE', 'None', "Don't use a distance model")
 ]
-
 
 _enum_object_property_types = [
     ('GAME', 'Game Property', 'Edit Game Property'),
     ('ATTR', 'Attribute', 'Edit Internal Attribute (can be used in materials)')
 ]
-
 
 _enum_2d_filters = [
     ('FXAA', 'FXAA', 'Fast Anti-Aliasing'),
@@ -161,53 +157,26 @@ _enum_2d_filters = [
     ('MIST', 'Mist', 'Classic depth fog implementation')
 ]
 
-
 _enum_constraint_types = [
-    (
-        "bge.constraints.POINTTOPOINT_CONSTRAINT",
-        "Ball",
-        "Allow rotation around all axis"
-    ),
-    (
-        "bge.constraints.LINEHINGE_CONSTRAINT",
-        "Hinge",
-        "Work on one plane, allow rotations on one axis only"
-    ),
-    (
-        "bge.constraints.CONETWIST_CONSTRAINT",
-        "Cone Twist",
-        (
-            'Allow rotations around all axis with limits for the cone '
-            'and twist axis'
-        )
-    ),
-    (
-        "bge.constraints.GENERIC_6DOF_CONSTRAINT",
-        "Generic 6 DOF",
-        "No constraints by default, limits can be set individually"
-    )
+    ("bge.constraints.POINTTOPOINT_CONSTRAINT", "Ball",
+        "Allow rotation around all axis"),
+    ("bge.constraints.LINEHINGE_CONSTRAINT", "Hinge",
+        "Work on one plane, allow rotations on one axis only"),
+    ("bge.constraints.CONETWIST_CONSTRAINT", "Cone Twist",
+        "Allow rotations around all axis with limits for the cone and twist axis"),
+    ("bge.constraints.GENERIC_6DOF_CONSTRAINT", "Generic 6 DOF",
+        "No constraints by default, limits can be set individually")
 ]
-
 
 _enum_ik_mode_values = [
     ("None", "None", "Not set"),
-    (
-        "bge.logic.CONSTRAINT_IK_MODE_INSIDE",
-        "Inside",
-        "Keep the bone with IK Distance of target"
-    ),
-    (
-        "bge.logic.CONSTRAINT_IK_MODE_OUTSIDE",
-        "Outside",
-        "Keep the bone outside IK Distance of target"
-    ),
-    (
-        "bge.logic.CONSTRAINT_IK_MODE_ONSURFACE",
-        "On Surface",
-        "Keep the bone exactly at IK Distance of the target"
-    )
+    ("bge.logic.CONSTRAINT_IK_MODE_INSIDE", "Inside",
+        "Keep the bone with IK Distance of target"),
+    ("bge.logic.CONSTRAINT_IK_MODE_OUTSIDE", "Outside",
+        "Keep the bone outside IK Distance of target"),
+    ("bge.logic.CONSTRAINT_IK_MODE_ONSURFACE", "On Surface",
+        "Keep the bone exactly at IK Distance of the target")
 ]
-
 
 _enum_field_value_types = [
     ("STRING", "String", "A String"),
@@ -236,7 +205,6 @@ _enum_vehicle_axis = [
     ("ALL", "All", "Apply to all wheels")
 ]
 
-
 _enum_loop_count_values = [
     (
         "ONCE",
@@ -260,99 +228,50 @@ _enum_loop_count_values = [
 _enum_readable_member_names = [
     ("worldPosition", "Position (Global)", "The World Position of the object"),
     ("localPosition", "Position (Local)", "The local position of the object"),
-    (
-        "worldOrientation",
-        "Rotation (Global)",
-        "The World Orientation of the object"
-    ), (
-        "localOrientation",
-        "Rotation (Local)",
-        "The local orientation of the object"
-    ), (
-        "worldLinearVelocity",
-        "Linear Velocity (Global)",
-        "The local linear velocity of the object"
-    ), (
-        "localLinearVelocity",
-        "Linear Velocity (Local)",
-        "The local linear velocity of the object"
-    ), (
-        "worldAngularVelocity",
-        "Angular Velocity (Global)",
-        "The local angular velocity of the object"
-    ), (
-        "localAngularVelocity",
-        "Angular Velocity (Local)",
-        "The local angular velocity of the object"
-    ), (
-        "worldTransform",
-        "Transform (Global)",
-        (
-            'The World Transform of the '
-            'object'
-        )
-    ), (
-        "localTransform",
-        "Transform (Local)",
-        (
-            'The local transform of the '
-            'object'
-        )
-    ),
+    ("worldOrientation", "Rotation (Global)",
+        "The World Orientation of the object"),
+    ("localOrientation", "Rotation (Local)",
+        "The local orientation of the object"),
+    ("worldLinearVelocity", "Linear Velocity (Global)",
+        "The local linear velocity of the object"),
+    ("localLinearVelocity", "Linear Velocity (Local)",
+        "The local linear velocity of the object"),
+    ("worldAngularVelocity", "Angular Velocity (Global)",
+        "The local angular velocity of the object"),
+    ("localAngularVelocity", "Angular Velocity (Local)",
+        "The local angular velocity of the object"),
+    ("worldTransform", "Transform (Global)",
+        "The World Transform of the object"),
+    ("localTransform", "Transform (Local)",
+        "The local transform of the object"),
     ("worldScale", "World Scale", "The global scale of the object"),
     ("localScale", "Local Scale", "The local scale of the object"),
     ("name", "Name", "The name of the object"),
     ("color", "Color", "The solid color of the object"),
-    (
-        "visible",
-        "Visibility",
-        "True if the object is set to visible, False if it is set of invisible"
-    )
+    ("visible", "Visibility",
+        "True if the object is set to visible, False if it is set of invisible")
 ]
 
 _enum_writable_member_names = [
     ("color", "Color", "The solid color of the object"),
     ("worldPosition", "Position (Global)", "The World Position of the object"),
     ("localPosition", "Position (Local)", "The local position of the object"),
-    (
-        "worldOrientation",
-        "Rotation (Global)",
-        "The World Orientation of the object"
-    ), (
-        "localOrientation",
-        "Rotation (Local)",
-        "The local orientation of the object"
-    ), (
-        "worldLinearVelocity",
-        "Linear Velocity (Global)",
-        "The local linear velocity of the object"
-    ), (
-        "localLinearVelocity",
-        "Linear Velocity (Local)",
-        "The local linear velocity of the object"
-    ), (
-        "worldAngularVelocity",
-        "Angular Velocity (Global)",
-        "The local rotational velocity of the object"
-    ), (
-        "localAngularVelocity",
-        "Angular Velocity (Local)",
-        "The local rotational velocity of the object"
-    ), (
-        "worldTransform",
-        "Transform (Global)",
-        (
-            'The World Transform of the '
-            'object'
-        )
-    ), (
-        "localTransform",
-        "Transform (Local)",
-        (
-            'The local transform of the '
-            'object'
-        )
-    ),
+    ("worldOrientation", "Rotation (Global)",
+        "The World Orientation of the object"),
+    ("localOrientation", "Rotation (Local)",
+        "The local orientation of the object"),
+    ("worldLinearVelocity", "Linear Velocity (Global)",
+        "The local linear velocity of the object"),
+    ("localLinearVelocity", "Linear Velocity (Local)",
+        "The local linear velocity of the object"),
+    ("worldAngularVelocity", "Angular Velocity (Global)",
+        "The local rotational velocity of the object"),
+    ("localAngularVelocity", "Angular Velocity (Local)",
+        "The local rotational velocity of the object"),
+    ("worldTransform", "Transform (Global)",
+        "The World Transform of the object"),
+    ("localTransform", "Transform (Local)",
+        "The local transform of the object"),
     ("worldScale", "Scale", "The global scale of the object")
 ]
 
@@ -365,14 +284,8 @@ _enum_mouse_buttons = [
 _enum_vsync_modes = [
     ("bge.render.VSYNC_OFF", "Off", "Disable Vsync"),
     ("bge.render.VSYNC_ON", "On", "Enable Vsync"),
-    (
-        "bge.render.VSYNC_ADAPTIVE",
-        "Adaptive",
-        (
-            'Enable adaptive Vsync '
-            '(if supported)'
-        )
-    )
+    ("bge.render.VSYNC_ADAPTIVE", "Adaptive",
+        "Enable adaptive Vsync (if supported)")
 ]
 
 _enum_string_ops = [
@@ -381,43 +294,15 @@ _enum_string_ops = [
     ("2", "Infix", "Insert A before String, B after String."),
     ("3", "Remove Last", "Remove Last Character from String"),
     ("4", "Remove First", "Remove First Character from String"),
-    (
-        "5",
-        "Replace",
-        'Replace all occurences of A with B'
-    ),
+    ("5", "Replace", "Replace all occurences of A with B"),
     ("6", "Upper Case", "Convert to Upper Case"),
     ("7", "Lower Case", "Convert to Lower Case"),
-    (
-        "8",
-        "Remove Range",
-        'Remove characters from index A to index B'
-    ),
-    (
-        "9",
-        "Insert At",
-        "Insert A at index B"
-    ),
-    (
-        "10",
-        "Length",
-        "Character Count (returns a Number)"
-    ),
-    (
-        "11",
-        "Substring",
-        "Characters between index A and index B"
-    ),
-    (
-        "12",
-        "First Index Of",
-        "Position of the first occurence of A"
-    ),
-    (
-        "13",
-        "Last Index Of",
-        "Position of the last occurence of A"
-    )
+    ("8", "Remove Range", "Remove characters from index A to index B"),
+    ("9", "Insert At", "Insert A at index B"),
+    ("10", "Length", "Character Count (returns a Number)"),
+    ("11", "Substring", "Characters between index A and index B"),
+    ("12", "First Index Of", "Position of the first occurence of A"),
+    ("13", "Last Index Of", "Position of the last occurence of A")
 ]
 
 _enum_quality_levels = [
@@ -457,7 +342,6 @@ _enum_logic_operators = [
     ("5", "Less or Equal", "A less or equal to B")
 ]
 
-
 _enum_controller_stick_operators = [
     ("0", "Left Stick", "Left Stick Values"),
     ("1", "Right Stick", "Right Stick Values")
@@ -472,7 +356,6 @@ _enum_vrcontroller_trigger_operators = [
     ("0", "Left", "Left Controller Values"),
     ("1", "Right", "Right Controller Values")
 ]
-
 
 _enum_controller_buttons_operators = [
     ("0", "A / Cross", "A / Cross Button"),
@@ -491,48 +374,31 @@ _enum_controller_buttons_operators = [
     ("14", "D-Pad Right", "D-Pad Right Button")
 ]
 
-
 _enum_distance_checks = [
     ("0", "AB = Dist", "AB Distance equal to Dist [Integer value 0]"),
     ("1", "AB != Dist", "AB Distance not equal to Dist [Integer value 1]"),
     ("2", "AB > Dist", "AB Distance greater than Dist [Integer value 2]"),
     ("3", "AB < Dist", "AB Distance less than Dist [Integer value 3]"),
-    (
-        "4",
-        "AB >= Dist",
-        "AB Distance greater than or equal to Dist [Integer value 4]"
-    ),
-    (
-        "5",
-        "AB <= Dist",
-        "AB Distance less than or equal to Dist [Integer value 5]"
-    ),
+    ("4", "AB >= Dist",
+        "AB Distance greater than or equal to Dist [Integer value 4]"),
+    ("5", "AB <= Dist",
+        "AB Distance less than or equal to Dist [Integer value 5]"),
 ]
-
 
 _enum_play_mode_values = [
     ("bge.logic.KX_ACTION_MODE_PLAY", "Play", "Play the action once"),
     ("bge.logic.KX_ACTION_MODE_LOOP", "Loop", "Loop the action"),
-    (
-        "bge.logic.KX_ACTION_MODE_PING_PONG",
-        "Ping Pong",
-        "Play the action in one direction then in the opposite one"
-    ),
+    ("bge.logic.KX_ACTION_MODE_PING_PONG", "Ping Pong",
+        "Play the action in one direction then in the opposite one"),
     ("bge.logic.KX_ACTION_MODE_PLAY + 3", "Play Stop", "Play the action once"),
     ("bge.logic.KX_ACTION_MODE_LOOP + 3", "Loop Stop", "Loop the action"),
-    (
-        "bge.logic.KX_ACTION_MODE_PING_PONG + 3",
-        "Ping Pong Stop",
-        "Play the action in one direction then in the opposite one"
-    )
+    ("bge.logic.KX_ACTION_MODE_PING_PONG + 3", "Ping Pong Stop",
+        "Play the action in one direction then in the opposite one")
 ]
 
 _enum_blend_mode_values = [
-    (
-        "bge.logic.KX_ACTION_BLEND_BLEND",
-        "Blend",
-        "Blend layers using linear interpolation"
-    ),
+    ("bge.logic.KX_ACTION_BLEND_BLEND", "Blend",
+        "Blend layers using linear interpolation"),
     ("bge.logic.KX_ACTION_BLEND_ADD", "Add", "Adds the layer together")
 ]
 
@@ -543,15 +409,16 @@ _enum_spawn_types = [
 ]
 
 _serialize_types = [
-    ("builtin", "Built-In", "Serialize Built-In data type (int, float, bool, dict, etc.)"),
+    ("builtin", "Built-In",
+        "Serialize Built-In data type (int, float, bool, dict, etc.)"),
     ("Vec2", "2D Vector", "Serialize a 2D Vector"),
     ("Vec3", "3D Vector", "Serialize a 3D Vector"),
     ("Vec4", "4D Vector", "Serialize a 4D Vector"),
     ("Mat3", "3x3 Matrix", "Serialize a 3x3 Matrix"),
     ("Mat4", "4x4 Matrix", "Serialize a 4x4 Matrix"),
-    ("GameObj", "Game Object", "Serialize a Game Object (Note: Not all data can be serialized)")
+    ("GameObj", "Game Object",
+        "Serialize a Game Object (Note: Not all data can be serialized)")
 ]
-
 
 _enum_msg_types = [
     ("INFO", "Info", "Will print the message in white (on-screen console)"),
